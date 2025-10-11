afce9de6f1e2cc1073c6cf7745e0dab7b88088c3
Tanya Mandloi05 <tanyamandloi2003@gmail.com>
2025-10-11 12:33:16 +0530

    learn file handling in python

---
+++ file.py
@@ -0,0 +1,39 @@
+# with open(r"C:\Users\admin\OneDrive\Desktop\js\index.js", "r", encoding="utf-8") as f:
+#     data = f.read()
+#     print(data)
+#     f.close()
+
+# f = open("demo.txt", "r")
+# data = f.read()
+# print(data)
+
+# f = open("demo.txt", "r")
+# line1 = f.readline()
+# line2 = f.readline()
+# print(line1)
+# print(line2)
+
+# f = open("demo.txt", "w")
+# f.write("i am learning aiml ")
+# f.write("\n after that i will practice node js interview")
+# f.close()
+
+# f = open("demo.txt", "a")
+# f.write("\n i am also making a project")
+
+# f = open("sample.txt", "w")
+# f.close()
+
+# f = open("sample.txt", "r+")
+# f.write("hello")
+
+# f = open("sample.txt", "w+")
+# print(f.read())
+# f.write("hello from tanya")
+
+# f = open("sample.txt", "a+")
+# print(f.read())
+# f.write("\n hello again")
+
+import os
+os.remove("sample.txt")
